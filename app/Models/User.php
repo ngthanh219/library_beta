f12c1828c84244f3cde16cb3fbca9d2595451a77
Chanh_Ubuntu2020 <quangsoi99@gmail.com>
2021-01-12 15:30:00 +0700

    Update DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable
 
     public function hasPermission(Permission $permission)
     {
-        $check = !!optional(optional($this->role)->permissions)->contains($permission);
+        $check = optional(optional($this->role)->permissions)->contains($permission);
         
         return $check;
     }
